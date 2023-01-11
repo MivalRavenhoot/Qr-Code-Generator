0b38258f483ede3945b2852a047acf02663ffa63
Real96 <chicco.gaeta@gmail.com>
2023-01-11 13:18:44 +0100

    Rename echo function

--- src/telegramPyBot.py
+++ src/telegramPyBot.py
@@ -6,13 +6,13 @@ async def start(update, context):
 async def help(update, context):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Usage:\nwrite the string you want to convert into a QR code image")
 
-async def echo(update, context):
+async def qr(update, context):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
 if __name__ == '__main__':
     token = open("botToken.txt", "r").readline()
     bot = ApplicationBuilder().token(token).build()
-    message_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    message_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), qr)
     
     bot.add_handler(CommandHandler('start', start))
     bot.add_handler(CommandHandler('help', help))
