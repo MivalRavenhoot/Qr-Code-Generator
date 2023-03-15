958a65c86e21506aae0b4965d928674c824ac9b1
Real96 <chicco.gaeta@gmail.com>
2023-03-15 16:21:16 +0100

    Modify comments above functions

--- src/telegram_py_bot.py
+++ src/telegram_py_bot.py
@@ -16,25 +16,27 @@ def QR(text):
     img = qr.make_image(fill_color="black", back_color="white")
     img.save("qrcode.png")
 
-#bot telegram basic commands
+# telegram bot start command handler
 async def start(update, context):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to the QR generator bot!\nSend /help to see the usage")
 
+# telegram bot help command handler
 async def help(update, context):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Usage:\nwrite the string you want to convert into a QR code image")
 
+# telegram bot qr code image sender function
 async def qr_generator(update, context):
     QR(update.message.text)
 
     with open('qrcode.png', 'rb') as qr_image:
         await context.bot.send_photo(chat_id=update.effective_chat.id, photo=qr_image)
 
-#telegram bot start and token validation
+# telegram bot start and token validation
 if __name__ == '__main__':
     with open("bot_token.txt", "r", encoding="utf8") as token:
         bot = ApplicationBuilder().token(token.readline()).build()
 
-#User message management and command recognition
+# user messages management and command recognition
     message_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), qr_generator)
 
     bot.add_handler(CommandHandler('start', start))
