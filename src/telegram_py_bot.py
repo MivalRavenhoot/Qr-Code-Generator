9229a567070072d012aa6d4302518c529d0eb4aa
Antonello2 <antonelloromanelli@hotmail.it>
2023-03-15 13:38:38 +0100

    Migrating qr function in the main code + comment

--- src/telegram_py_bot.py
+++ src/telegram_py_bot.py
@@ -1,6 +1,24 @@
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
 from qr_code_generator import QR
+import qrcode
 
+# Qrcode generation function in png format
+def QR(text):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=15,
+        border=4,
+    )
+
+    qr.add_data(text)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save("url_qrcode.png")
+
+
+#bot telegram basic commands
 async def start(update, context):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to the QR generator bot!\nSend /help to see the usage")
 
@@ -11,10 +29,12 @@ async def qr(update, context):
     QR(update.message.text)
     await context.bot.send_photo(chat_id=update.effective_chat.id, photo=open('url_qrcode.png', 'rb'))
 
+#telegram bot start and token validation
 if __name__ == '__main__':
     with open("bot_token.txt", "r", encoding="utf8") as token:
         bot = ApplicationBuilder().token(token.readline()).build()
 
+#User message management and command recognition
     message_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), qr)
 
     bot.add_handler(CommandHandler('start', start))
