f666ca76c1025ba81dfca983d3997290274fdc37
Antonello2 <antonelloromanelli@hotmail.it>
2023-03-16 18:59:55 +0100

    Pytest Start Function implemented

--- tests/test_start.py
+++ tests/test_start.py
@@ -1,2 +1,17 @@
 from src.telegram_py_bot import start
+from unittest.mock import AsyncMock, Mock
+import pytest
 
+@pytest.mark.asyncio
+async def test_start():
+    update = Mock()
+    context = Mock()
+    context.bot.send_message = AsyncMock()
+    context.bot.send_message.return_value = "some value"
+
+    await start(update, context)
+
+    context.bot.send_message.assert_called_once_with(
+        chat_id=update.effective_chat.id,
+        text="Welcome to the QR generator bot!\nSend /help to see the usage",
+    )
